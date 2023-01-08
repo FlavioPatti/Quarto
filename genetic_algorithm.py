e1ba62a0f037707d6ef517bbb580035381d88c6d
antoniodecinque99 <101060871+antoniodecinque99@users.noreply.github.com>
2023-01-08 14:46:07 +0100

    Update genetic_algorithm.py

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -3,6 +3,7 @@ from collections import namedtuple
 import random
 from copy import deepcopy
 import quarto
+import math
 
 Individual = namedtuple("Individual", ["genome", "fitness"])
 # Genome -> array of 8 elements (4 figures + 4 positions)
@@ -10,51 +11,63 @@ Individual = namedtuple("Individual", ["genome", "fitness"])
 BOARD_SIZE = 4
 GENOME_SIZE = BOARD_SIZE * 2
 POPULATION_SIZE = 100
-NUM_GENERATIONS = 50
+NUM_GENERATIONS = 20
 OFFSPRING_SIZE = 50
 TOURNAMENT_SIZE = 5
 GENETIC_OPERATOR_RANDOMNESS = 0.5
-CROSSOVER_THRESHOLD = 0.3
-MUTATION_THRESHOLD = 0.3
+CROSSOVER_THRESHOLD = 0.2
+MUTATION_THRESHOLD = 0.2
 
-    
+
 class GeneticAlgorithm():
 
     def __init__(self, current_game: quarto.Quarto):
         self.current_game = current_game
         
-    def convert_coordinates(row, col):
-        return 4 * row + col
+    def unplace(self, x: int, y: int):
+        self.current_game._Quarto__board[y, x] = -1
     
-    def available_positions(self, genome=None):
+    def coordinate_tuple_to_index(self, row, col):
+        return 4 * row + col
+
+    def coordinate_index_to_tuple(self, index):
+        col = index % BOARD_SIZE
+        row = math.floor(index / BOARD_SIZE)
+        return (row,col)
+    
+    def available_positions(self, genome: list = None):
         list_available_positions = []
 
-        for row in self.current_game.BOARD_SIDE:
-            for col in self.current_game.BOARD_SIDE:
-                if self.current_game.board[row,col] == -1:
-                    coord = self.convert_coordinates(row, col)
+        for row in range(self.current_game.BOARD_SIDE):
+            for col in range(self.current_game.BOARD_SIDE):
+                if self.current_game._Quarto__board[col,row] == -1:
+                    coord = self.coordinate_tuple_to_index(row, col)
                     list_available_positions.append(coord)
+        #print(list_available_positions)
 
         if genome is not None:
-            for i in range(GENOME_SIZE/2, GENOME_SIZE):
-                list_available_positions.remove(genome[i])
+            for i in range(GENOME_SIZE//2, GENOME_SIZE):
+                if (genome[i] in list_available_positions) and len(list_available_positions) > 0:
+                    list_available_positions.remove(genome[i])
         
         return list_available_positions
     
     
-    def available_pieces(self, genome=None):
-        list_available_pieces = (x for x in range(0,16))
+    def available_pieces(self, genome: list = None):
+        list_available_pieces = list(range(16))
         #list_available_pieces.remove(self.current_game.selected_piece_index)
         
         if genome is not None:
-            for i in range(0, GENOME_SIZE/2):
-                list_available_pieces.remove(genome[i])
+            for i in range(0, GENOME_SIZE//2):
+                if (genome[i] in list_available_pieces):
+                    list_available_pieces.remove(genome[i])
 
-        for row in self.current_game.BOARD_SIDE:
-            for col in self.current_game.BOARD_SIDE:
-                current_piece = self.current_game.board[row,col]
-                if current_piece != -1:
+        for row in range(self.current_game.BOARD_SIDE):
+            for col in range(self.current_game.BOARD_SIDE):
+                current_piece = self.current_game._Quarto__board[col,row]
+                if current_piece != -1 and len(list_available_pieces) > 0:
                     list_available_pieces.remove(current_piece)
+
                     
         return list_available_pieces
     
@@ -66,18 +79,20 @@ class GeneticAlgorithm():
     """generate our initial population"""
     def init_population(self):
         population = []
-        for _ in range(POPULATION_SIZE):
-            for _ in range(GENOME_SIZE):
-                genome = []
-                genome.append(self.current_game.selected_piece_index)
+        for p in range(POPULATION_SIZE):
+            for g in range(GENOME_SIZE):
+                genome = [-1]*GENOME_SIZE
+                genome[0] = self.current_game._Quarto__selected_piece_index
                 
-                for i in range(1,GENOME_SIZE/2):
-                    list_available_pieces = self.list_available_pieces(genome)
-                    genome.append(random.choice(list_available_pieces))
+                for i in range(1,GENOME_SIZE//2):
+                    list_available_pieces = self.available_pieces(genome)
+                    if (len(list_available_pieces) > 0):
+                        genome[i] = random.choice(list_available_pieces)
                     
-                for i in range(GENOME_SIZE/2,GENOME_SIZE):
-                    list_available_positions = self.list_available_positions(genome)
-                    genome.append(random.choice(list_available_positions))
+                for i in range(GENOME_SIZE//2,GENOME_SIZE):
+                    list_available_positions = self.available_positions(genome)
+                    if (len(list_available_positions) > 0):
+                        genome[i] = random.choice(list_available_positions)
                 
                 population.append(Individual(genome, self.compute_fitness(genome)))
         
@@ -93,24 +108,27 @@ class GeneticAlgorithm():
                 new_genome.append(genome_2[i])
         return new_genome
 
-    """"""
+    """change pieces and positions according to a certain threshold"""
     def mutation(self, genome): 
         new_genome = deepcopy(genome)
-        for i in range(1, GENOME_SIZE/2): # mutate pieces
+        for i in range(1, GENOME_SIZE//2): # mutate pieces
             if (random.randint(0,1) > MUTATION_THRESHOLD):
                 available_pieces = self.available_pieces(new_genome)
-                new_genome[i] = (random.choice(available_pieces))
+                if (len(available_pieces) > 0):
+                    new_genome[i] = (random.choice(available_pieces))
 
-        for i in range(GENOME_SIZE/2, GENOME_SIZE): # mutate positions
+        for i in range(GENOME_SIZE//2, GENOME_SIZE): # mutate positions
             if (random.randint(0,1) > MUTATION_THRESHOLD):
                 available_positions = self.available_positions(new_genome)
-                new_genome[i] = (random.choice(available_positions))
+                if (len(available_positions) > 0):
+                    new_genome[i] = (random.choice(available_positions))
                 
         return new_genome
 
     def my_move(self):
         population = self.init_population()
         for g in range(NUM_GENERATIONS):
+            #print(g)
             offspring = list()
             for i in range(OFFSPRING_SIZE):
                 if random.random() < GENETIC_OPERATOR_RANDOMNESS:                         
@@ -121,61 +139,77 @@ class GeneticAlgorithm():
                     p2 = self.tournament(population)
                     o = self.cross_over(p1.genome, p2.genome)            
                 f = self.compute_fitness(o)                                                          
-                offspring.append(Individual(o, f))                 
+                offspring.append(Individual(o, f))  
+           
             population += offspring
-            population = set(population)  #remove duplicate
-            population = list(population)  
+
+            #population = set(population)  #remove duplicate
+            #population = list(population) 
+
             population = sorted(population, key=lambda i: i[1], reverse = True)[:POPULATION_SIZE]
             
             best_genome = population[0][0]
         
+        #print(*population, sep="\n")
         piece_to_give = best_genome[1]
-        position_to_play = best_genome[4]
+        position_to_play = self.coordinate_index_to_tuple(best_genome[4])
+        #print((piece_to_give, position_to_play))
         return (piece_to_give, position_to_play)
     
-    def check_winning_move(self):
-        cwmh = self.current_game.check_horizontal()
-        cwmv = self.current_game.check_vertical()
-        cwmd = self.current_game.check_diagonal()
+    
+    def check_winning_move(self, game):
+        cwmh = game._Quarto__check_horizontal()
+        cwmv = game._Quarto__check_vertical()
+        cwmd = game._Quarto__check_diagonal()
         if cwmh != -1 or cwmv != -1 or cwmd != -1:
             return 1
         else:
             return 0
     
-    def winning_pos(self, current_figure):
-        self.selected_piece_index = current_figure
-        list_winning_move = []
+    def winning_pos(self, current_piece):
+        old_piece_index = self.current_game._Quarto__selected_piece_index
+        self.current_game._Quarto__selected_piece_index = current_piece
+        
+        list_winning_moves = []
         for row in range(BOARD_SIZE):
             for col in range(BOARD_SIZE):
-                self.current_place(col, row)
-                if self.check_winning_move():
-                    coord = self.convert_coordinates(row,col)
-                    list_winning_move.append(coord)
-                self.current_game.unplace(col,row)
-        return list_winning_move
+                game_copy = deepcopy(self.current_game)
+                game_copy.place(row, col)
+
+                if self.check_winning_move(game_copy):
+                    coord = self.coordinate_tuple_to_index(row,col)
+                    list_winning_moves.append(coord)
+                #self.unplace(col,row)
+        
+        self.current_game._Quarto__selected_piece_index = old_piece_index
+        return list_winning_moves
     
     def compute_fitness (self,genome):
         winning_reward = 10
         losing_reward = 10
+        noCombo = 1
         tot_reward = 0
                 
-        for index in range(GENOME_SIZE/2):
-            figure = genome[index]
-            coordinates = genome[GENOME_SIZE/2-index]
+        for index in range(GENOME_SIZE//2):
+            piece = genome[index]
+            coordinates = genome[GENOME_SIZE//2-index]
             myTurn = True
             
-            list = self.winning_pos(figure)
+            list = self.winning_pos(piece)
+            gameOver = True if len(list) > 0 else False
             
-            if myTurn:
-                if list.count(coordinates) > 0:
-                    tot_reward += winning_reward * (GENOME_SIZE/2-index)
+            if gameOver:
+                if myTurn:
+                    if list.count(coordinates) > 0:
+                        tot_reward += winning_reward * (GENOME_SIZE//2-index)
+                    #else: tot_reward -= 5 
                 else:
-                    tot_reward -= 5 
+                    if list.count(coordinates) > 0:
+                        tot_reward -= losing_reward * (GENOME_SIZE//2-index)
+                    #else: tot_reward += 5  
             else:
-                if list.count(coordinates) > 0:
-                    tot_reward -= losing_reward * (GENOME_SIZE/2-index)
-                else: tot_reward += 5  
-                
+                tot_reward -= noCombo
+
             myTurn = not myTurn
             
         return tot_reward
