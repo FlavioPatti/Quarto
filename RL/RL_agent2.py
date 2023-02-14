c6a43f75204906f9af8bea28a70afb5615f5f8e5
Leo-Rola <leorola998@gmail.com>
2023-02-14 13:22:12 +0100

    final pushhhhh

--- RL/RL_agent2.py
+++ RL/RL_agent2.py
@@ -5,8 +5,10 @@ import copy
 import pickle
 import math
 import sys
-#Implementation of a custom RL with some elements that are similar to a minmax
+'''Implementation of a custom RL with some elements that are similar to a minmax'''
 class RL_Agent2(quarto.Player):
+    '''In our case, to compress informations, the action that place a piece and the action that choose a piece for the opponent are unified,
+    So the upper bound of the number of the possible actions will be 16(number of places)*16(number of pieces). That's the reason of an action space of 256'''
     action_space = 256
 
     def __init__(self, quarto:quarto.Quarto, train_mode=True, pretrained=False):
@@ -21,7 +23,8 @@ class RL_Agent2(quarto.Player):
             self.action=None
 
         if self.pretrained==True:
-            self.load()     
+            self.load() 
+
 
     def choose_piece(self):
         if self.get_game().get_selected_piece()==-1:
@@ -43,7 +46,6 @@ class RL_Agent2(quarto.Player):
             for xp in yp:
                 state.append(xp)
         state.append(self.get_game().get_selected_piece())
-        #print(state)
         if self.train_mode:
             current_action=self.update_state_history(state)
         else:
@@ -53,10 +55,11 @@ class RL_Agent2(quarto.Player):
         pos=current_action//16
         y=pos//4
         x=pos%4
-        #print(x , "-" , y)
         return (x,y)
 
     def make_and_get_action_values(self, state, possActions):
+        '''If I'm in training phase if I've never visited a state, I create it initializing the rewards to couples of 0s
+        If i'm in test phase I take the rewards of the actions of a state, and if that state isn't in the q-table, I return a list of couples of 0s as reward'''
         state=tuple(state)
         def_list=[]
         if self.train_mode==True:
@@ -64,34 +67,35 @@ class RL_Agent2(quarto.Player):
         return self.q.get(state, np.zeros((self.action_space,2)))[possActions]
 
     def is_terminal(self):
-        '''returns True if the state is terminal'''
+        '''CURRENTLY UNUSED
+        returns True if the state is terminal'''
         return self.get_game().check_finished() or self.get_game().check_winner()>=0
 
     def getActions(self, state):
         '''returns a list of possible actions for a given state'''
-        #if self.is_terminal():
-        #    return [None]
         count=state.count(-1)
+        '''Optimization to explore less states
+        Basing on simmetries, if I begin for first, any piece I choose is the same '''
         if count==17:
-            return [0]#,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]  #PROVA OTTIMIZZAZIONEEEEEEEEE
+            return [0]#,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15] 
         
         all_pieces={ x for x in range(len(state)-1)}
         available_pieces=list(all_pieces - set(state))
-        # per evitare bug quando si sta per fare l'ultima mossa che porterà a un draw!
+        #To avoid a bug when I'm about to do the last move that will lead to a draw
         if available_pieces==[]:
             available_pieces.append(0)
+        '''Optimization to explore less states
+        Basing on simmetries, if I begin for second, any place I choose to put the piece chosen by my opponent
+        is equivalent to putting the piece in the position 0 or 1'''
         if count==16:
             available_positions=[0,1]
         else:
             available_positions=[]
-            #print("available pieces: ", available_pieces)
             for i, o in enumerate(state):
                 if o==-1:
                     available_positions.append(i)
-            #print("available positions: ", available_positions)
         possible_actions = [
             16 * pos + piece for pos in available_positions for piece in available_pieces]
-        #print("possible actions: ", possible_actions)
         return possible_actions
 
 
@@ -153,15 +157,17 @@ class RL_Agent2(quarto.Player):
             
         
       
-    # Choose the action and updates the state hystory 
+     
     def update_state_history(self, state):
-    
+        '''Choose the action and updates the state hystory'''
         current_action = self.policy(state)
         self.state_history.append((state, current_action))
 
         return current_action
 
     def learn(self, winner):
+        """Called only at the end of a match and only for training.
+        Spread the rewards of the final states"""
         prev_state, prev_action =self.state_history[-1]
         if self.q[tuple(prev_state)][prev_action][1]==0:
           
@@ -180,12 +186,13 @@ class RL_Agent2(quarto.Player):
 
 
     def save(self):
-        # Save the q-table on the disk for future use 
+        '''Save the q-table on the disk for future use'''
         with open('player.bin', 'wb') as f:
             pickle.dump(dict(self.q), f, protocol=4)
       
             
 
     def load(self):
+        '''load the q-table already saved if I don't have to train from scratch'''
         with open('player.bin', 'rb') as f:
             self.q=pickle.load(f)
